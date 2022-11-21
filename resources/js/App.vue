ff7e76ed44a18ca7119d7b96b4377357efe5184a
Valeriy <valera.komar.1999@gmail.com>
2022-11-21 19:18:45 +0200

    Integrate template

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,49 @@
 <template>
-    <h1>This our root component</h1>
+    <div id="wrapper">
+        <!-- sidebar -->
+        <div class="sidebar">
+            <span class="closeButton">&times;</span>
+            <p class="brand-title"><a href="">Alphayo Blog</a></p>
+
+            <div class="side-links">
+                <ul>
+                    <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
+                    <li><router-link :to="{ name: 'Blog' }">Blog</router-link></li>
+                    <li><router-link :to="{ name: 'About' }">About</router-link></li>
+                    <li><router-link :to="{ name: 'Contact' }">Contact</router-link></li>
+                </ul>
+            </div>
+
+            <!-- sidebar footer -->
+            <footer class="sidebar-footer">
+                <div>
+                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                    <a href=""><i class="fab fa-instagram"></i></a>
+                    <a href=""><i class="fab fa-twitter"></i></a>
+                </div>
+
+                <small>2022 Komar Natalia Blog</small>
+            </footer>
+        </div>
+        <!-- Menu Button -->
+        <div class="menuButton">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+        <!-- main -->
+        <main class="container">
+            <!-- render components on the page visited-->
+        </main>
+        <router-view></router-view>
+        <!-- Main footer -->
+        <footer class="main-footer">
+            <div>
+                <a href=""><i class="fab fa-facebook-f"></i></a>
+                <a href=""><i class="fab fa-instagram"></i></a>
+                <a href=""><i class="fab fa-twitter"></i></a>
+            </div>
+            <small>2022 Komar Natalia Blog</small>
+        </footer>
+    </div>
 </template>
